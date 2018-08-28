33dc6bfc86a30d2d66640980ae0433e37f67b46f
e <xieziyang@unpay.com>
2018-08-28 16:51:41 +0800

    test

--- springboot/src/main/java/com/bsworld/springboot/start/aop/AopServiceImpl.java
+++ springboot/src/main/java/com/bsworld/springboot/start/aop/AopServiceImpl.java
@@ -18,7 +18,6 @@ public class AopServiceImpl implements AopService {
 
     private static List<User> list = null;
 
-
     static {
         list = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
